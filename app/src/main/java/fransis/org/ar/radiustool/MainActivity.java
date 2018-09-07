997dcd78bf7b072cbb78c74f36679d76d4447540
francisco <gianafrancisco@gmail.com>
2018-09-07 12:16:10 -0300

    Added splashscreen

--- app/src/main/java/fransis/org/ar/radiustool/MainActivity.java
+++ app/src/main/java/fransis/org/ar/radiustool/MainActivity.java
@@ -52,6 +52,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
