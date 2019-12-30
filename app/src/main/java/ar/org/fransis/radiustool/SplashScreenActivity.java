9c033dca33f724f0256805394ec04f7fb51b9606
gianafrancisco@gmail.com <gianafrancisco@gmail.com>
2019-12-29 21:28:47 -0300

    splash screen

---
+++ app/src/main/java/ar/org/fransis/radiustool/SplashScreenActivity.java
@@ -0,0 +1,20 @@
+package ar.org.fransis.radiustool;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme);
+        setContentView(R.layout.activity_splash_screen);
+
+    }
+
+}
